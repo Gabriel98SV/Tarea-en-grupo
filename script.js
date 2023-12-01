60a9c6d52d20385d3dd961810ab3ac416eee89a3
Christian Zaldaña <chris.zaldana92@gmail.com>
2023-12-01 11:43:05 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function calcularIMC() {
+    // Para obtener los valores necesarios
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+
+    // Para ingresar datos validos
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        alert('Por favor, ingresa valores válidos para peso y altura.');
+        return;
+    }
+
+    // Para calcular el IMC
+    const imc = peso / ((altura / 100) ** 2);
+
+    // Resultado mostrado al usuario
+    document.getElementById('resultado-imc').innerText = `Tu IMC es: ${imc.toFixed(2)}`;
+}
